57138e773ae31432b70e70083d71c48346caee86
y0sh1DE <chainzzockt@gmail.com>
2020-04-25 22:09:36 +0200

    Small Tweaks

--- windows/Forms/frmConfigwizard.cs
+++ windows/Forms/frmConfigwizard.cs
@@ -30,17 +30,31 @@ namespace serwmImageUploader
 
         public Configuration Config { get => this._config; }
 
+        private bool isValidConfig()
+        {
+            // ToDo: more validation
+            if (tbxRemoteDirectory.Text[tbxRemoteDirectory.Text.Length - 1] != '/') return false;
+            return true;
+        }
+
         private void btnSaveRun_Click(object sender, EventArgs e)
         {
-            _config = new Configuration();
-            _config.Username = tbxUsername.Text;
-            _config.Password = tbxPassword.Text;
-            _config.Address = tbxServerAddress.Text;
-            _config.RemoteDirectory = tbxRemoteDirectory.Text;
-            _config.OpenImageAfterUpload = chkOpenAfterCreation.Checked;
-            _config.Save();
-            this.DialogResult = DialogResult.OK;
-            this.Close();
+            if(isValidConfig())
+            {
+                _config = new Configuration();
+                _config.Username = tbxUsername.Text;
+                _config.Password = tbxPassword.Text;
+                _config.Address = tbxServerAddress.Text;
+                _config.RemoteDirectory = tbxRemoteDirectory.Text;
+                _config.OpenImageAfterUpload = chkOpenAfterCreation.Checked;
+                _config.Save();
+                this.DialogResult = DialogResult.OK;
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Missing or wrong configuration details!","Error",MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
